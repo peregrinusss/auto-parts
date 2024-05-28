0c005f7698004207db1a54b799d369c2592bbf72
Danil <Daniellly@yandex.ru>
2024-05-28 03:16:00 +0300

    update project with nunjunks

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ const withMT = require("@material-tailwind/html/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = withMT({
-  content: ["./src/**/*.{html,js,php}"],
+  content: ["./src/**/*.{html,js,php,njk}"],
   darkMode: "class",
   theme: {
     colors: {
