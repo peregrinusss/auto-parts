b711d8ab32a44531a2972abc3ccfeb366c7dcaf9
Danil <Daniellly@yandex.ru>
2024-06-06 18:00:04 +0300

    add users page

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
-
-
 const modal = new GraphModal({
   isOpen: (modal) => {
     console.log(modal);
-    console.log('opened');
+    console.log("opened");
   },
 });
 
@@ -11,7 +9,7 @@ const accordeon = new HandyCollapse({
   nameSpace: "nested",
   closeOthers: false,
   isAnimation: true,
-  animationSpeed: 400
+  animationSpeed: 400,
 });
 
 // sidebar
@@ -53,9 +51,7 @@ if (tabs.length) {
 
     buttons.forEach((button) => {
       button.addEventListener("click", function () {
-        buttons.forEach((btn) =>
-          btn.classList.remove("tab-active")
-        );
+        buttons.forEach((btn) => btn.classList.remove("tab-active"));
         button.classList.add("tab-active");
       });
     });
@@ -72,7 +68,7 @@ if (paginations.length) {
 
     items.forEach((item, i) => {
       if (item.classList.contains("pagination-active")) {
-        index = i+1;
+        index = i + 1;
       }
     });
 
@@ -89,76 +85,84 @@ if (paginations.length) {
   });
 }
 
-
 // table content
 
-document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.querySelectorAll('[table-item-button]');
-  const modals = document.querySelectorAll('[table-item-modal]');
-  const closeButtons = document.querySelectorAll('[table-close-modal]');
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll("[table-item-button]");
+  const modals = document.querySelectorAll("[table-item-modal]");
+  const closeButtons = document.querySelectorAll("[table-close-modal]");
 
   buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      modals[index].classList.remove('hidden');
-      const modalContent = modals[index].querySelector('[table-modal-content]');
+    button.addEventListener("click", () => {
+      modals[index].classList.remove("hidden");
+      const modalContent = modals[index].querySelector("[table-modal-content]");
       requestAnimationFrame(() => {
-        modalContent.classList.remove('opacity-0', 'translate-y-14');
+        modalContent.classList.remove("opacity-0", "translate-y-14");
       });
-      document.body.classList.add('overflow-hidden');
+      document.body.classList.add("overflow-hidden");
     });
   });
 
   closeButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
       const modal = modals[index];
-      const modalContent = modal.querySelector('[table-modal-content]');
-      modalContent.classList.add('opacity-0', 'translate-y-14');
-      modalContent.addEventListener('transitionend', () => {
-        modal.classList.add('hidden');
-        document.body.classList.remove('overflow-hidden');
-      }, { once: true });
+      const modalContent = modal.querySelector("[table-modal-content]");
+      modalContent.classList.add("opacity-0", "translate-y-14");
+      modalContent.addEventListener(
+        "transitionend",
+        () => {
+          modal.classList.add("hidden");
+          document.body.classList.remove("overflow-hidden");
+        },
+        { once: true }
+      );
     });
   });
 
   modals.forEach((modal) => {
-    modal.addEventListener('click', (event) => {
-      if (!event.target.closest('[table-modal-content]')) {
-        const modalContent = modal.querySelector('[table-modal-content]');
-        modalContent.classList.add('opacity-0', 'translate-y-14');
-        modalContent.addEventListener('transitionend', () => {
-          modal.classList.add('hidden');
-          document.body.classList.remove('overflow-hidden');
-        }, { once: true });
+    modal.addEventListener("click", (event) => {
+      if (!event.target.closest("[table-modal-content]")) {
+        const modalContent = modal.querySelector("[table-modal-content]");
+        modalContent.classList.add("opacity-0", "translate-y-14");
+        modalContent.addEventListener(
+          "transitionend",
+          () => {
+            modal.classList.add("hidden");
+            document.body.classList.remove("overflow-hidden");
+          },
+          { once: true }
+        );
       }
     });
   });
 });
 
-
-
 //  graph-modal
 
-
-
-
-
 // gallery in modal
-document.addEventListener('DOMContentLoaded', function() {
-  const fileInput = document.getElementById('fileInput');
-  const previewContainer = document.getElementById('preview-container');
+document.addEventListener("DOMContentLoaded", function () {
+  const fileInput = document.getElementById("fileInput");
+  const previewContainer = document.getElementById("preview-container");
 
-  fileInput.addEventListener('change', function() {
+  fileInput.addEventListener("change", function () {
     const files = Array.from(fileInput.files);
 
-    files.forEach(file => {
+    files.forEach((file) => {
       const reader = new FileReader();
-      reader.onload = function(e) {
-        const preview = document.createElement('div');
-        preview.classList.add('rounded-xl', 'w-full', 'max-w-[300px]', 'border-4', 'border-solid', 'border-primary');
+      reader.onload = function (e) {
+        const preview = document.createElement("div");
+        preview.classList.add(
+          "rounded-xl",
+          "w-full",
+          "max-w-[300px]",
+          "border-4",
+          "border-solid",
+          "border-primary"
+        );
 
-        const img = document.createElement('img');
+        const img = document.createElement("img");
         img.src = e.target.result;
-        img.classList.add('rounded-[8px]', 'w-full', 'h-full', 'object-cover');
+        img.classList.add("rounded-[8px]", "w-full", "h-full", "object-cover");
 
         preview.appendChild(img);
         previewContainer.appendChild(preview);
@@ -170,45 +174,53 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 // mini-modals
-document.addEventListener('DOMContentLoaded', () => {
-  const topToggles = document.querySelectorAll('[minimodal-top-toggle]');
-  const topModals = document.querySelectorAll('[minimodal-top-content]');
+document.addEventListener("DOMContentLoaded", () => {
+  const topToggles = document.querySelectorAll("[minimodal-top-toggle]");
+  const topModals = document.querySelectorAll("[minimodal-top-content]");
 
   // Делегирование событий для переключателей
   topToggles.forEach((toggle, index) => {
-    toggle.addEventListener('click', () => {
+    toggle.addEventListener("click", () => {
       const modal = topModals[index];
       if (modal) {
-        if (modal.classList.contains('hidden')) {
-          modal.classList.remove('hidden');
+        if (modal.classList.contains("hidden")) {
+          modal.classList.remove("hidden");
           setTimeout(() => {
             requestAnimationFrame(() => {
-              modal.classList.remove('translate-y-14', 'opacity-0');
+              modal.classList.remove("translate-y-14", "opacity-0");
             });
           }, 10);
-          document.body.classList.add('overflow-hidden');
+          document.body.classList.add("overflow-hidden");
         } else {
-          modal.classList.add('opacity-0', 'translate-y-14');
-          modal.addEventListener('transitionend', () => {
-            modal.classList.add('hidden');
-            document.body.classList.remove('overflow-hidden');
-          }, { once: true });
+          modal.classList.add("opacity-0", "translate-y-14");
+          modal.addEventListener(
+            "transitionend",
+            () => {
+              modal.classList.add("hidden");
+              document.body.classList.remove("overflow-hidden");
+            },
+            { once: true }
+          );
         }
       }
     });
   });
 
   // Закрытие модальных окон при клике вне их содержимого
-  document.addEventListener('click', (event) => {
+  document.addEventListener("click", (event) => {
     topModals.forEach((modal, index) => {
       const toggle = topToggles[index];
       if (!modal.contains(event.target) && !toggle.contains(event.target)) {
-        if (!modal.classList.contains('hidden')) {
-          modal.classList.add('opacity-0', 'translate-y-14');
-          modal.addEventListener('transitionend', () => {
-            modal.classList.add('hidden');
-            document.body.classList.remove('overflow-hidden');
-          }, { once: true });
+        if (!modal.classList.contains("hidden")) {
+          modal.classList.add("opacity-0", "translate-y-14");
+          modal.addEventListener(
+            "transitionend",
+            () => {
+              modal.classList.add("hidden");
+              document.body.classList.remove("overflow-hidden");
+            },
+            { once: true }
+          );
         }
       }
     });
@@ -216,52 +228,59 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Предотвращение закрытия модального окна при клике внутри него
   topModals.forEach((modal) => {
-    modal.addEventListener('click', (event) => {
+    modal.addEventListener("click", (event) => {
       event.stopPropagation();
     });
   });
 });
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const bottomToggles = document.querySelectorAll('[minimodal-bottom-toggle]');
-  const bottomModals = document.querySelectorAll('[minimodal-bottom-content]');
+document.addEventListener("DOMContentLoaded", () => {
+  const bottomToggles = document.querySelectorAll("[minimodal-bottom-toggle]");
+  const bottomModals = document.querySelectorAll("[minimodal-bottom-content]");
 
   // Делегирование событий для переключателей
   bottomToggles.forEach((toggle, index) => {
-    toggle.addEventListener('click', () => {
+    toggle.addEventListener("click", () => {
       const modal = bottomModals[index];
       if (modal) {
-        if (modal.classList.contains('hidden')) {
-          modal.classList.remove('hidden');
+        if (modal.classList.contains("hidden")) {
+          modal.classList.remove("hidden");
           setTimeout(() => {
             requestAnimationFrame(() => {
-              modal.classList.remove('translate-y-full', 'opacity-0');
+              modal.classList.remove("translate-y-full", "opacity-0");
             });
           }, 10);
-          document.body.classList.add('overflow-hidden');
+          document.body.classList.add("overflow-hidden");
         } else {
-          modal.classList.add('opacity-0', 'translate-y-full');
-          modal.addEventListener('transitionend', () => {
-            modal.classList.add('hidden');
-            document.body.classList.remove('overflow-hidden');
-          }, { once: true });
+          modal.classList.add("opacity-0", "translate-y-full");
+          modal.addEventListener(
+            "transitionend",
+            () => {
+              modal.classList.add("hidden");
+              document.body.classList.remove("overflow-hidden");
+            },
+            { once: true }
+          );
         }
       }
     });
   });
 
   // Закрытие модальных окон при клике вне их содержимого
-  document.addEventListener('click', (event) => {
+  document.addEventListener("click", (event) => {
     bottomModals.forEach((modal, index) => {
       const toggle = bottomToggles[index];
       if (!modal.contains(event.target) && !toggle.contains(event.target)) {
-        if (!modal.classList.contains('hidden')) {
-          modal.classList.add('opacity-0', 'translate-y-full');
-          modal.addEventListener('transitionend', () => {
-            modal.classList.add('hidden');
-            document.body.classList.remove('overflow-hidden');
-          }, { once: true });
+        if (!modal.classList.contains("hidden")) {
+          modal.classList.add("opacity-0", "translate-y-full");
+          modal.addEventListener(
+            "transitionend",
+            () => {
+              modal.classList.add("hidden");
+              document.body.classList.remove("overflow-hidden");
+            },
+            { once: true }
+          );
         }
       }
     });
@@ -269,8 +288,75 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Предотвращение закрытия модального окна при клике внутри него
   bottomModals.forEach((modal) => {
-    modal.addEventListener('click', (event) => {
+    modal.addEventListener("click", (event) => {
       event.stopPropagation();
     });
+  });
+});
+
+// select
+document.addEventListener("DOMContentLoaded", function () {
+  function closeAllDropdowns() {
+    document.querySelectorAll(".dropdown-options").forEach((dropdown) => {
+      if (!dropdown.classList.contains("hidden")) {
+        closeDropdown(dropdown);
+      }
+    });
+  }
+
+  function openDropdown(dropdown) {
+    dropdown.classList.remove("opacity-0", "scale-95", "pointer-events-none");
+    dropdown.classList.add("opacity-100", "scale-100");
+  }
+
+  function closeDropdown(dropdown) {
+    dropdown.classList.remove("opacity-100", "scale-100");
+    dropdown.classList.add("opacity-0", "scale-95", "pointer-events-none");
+  }
+
+  document.querySelectorAll(".custom-select").forEach((customSelect) => {
+    const selectButton = customSelect.querySelector(".select-button");
+    const dropdownOptions = customSelect.querySelector(".dropdown-options");
+    const selectedOption = customSelect.querySelector(".selected-option");
+
+    // Set the first option as the default selected option
+    const firstOption = dropdownOptions.querySelector("li");
+    if (firstOption) {
+      selectedOption.textContent = firstOption.textContent;
+      firstOption.classList.add("text-primary", "font-bold");
+    }
+
+    selectButton.addEventListener("click", function (event) {
+      closeAllDropdowns();
+      if (dropdownOptions.classList.contains("opacity-0")) {
+        openDropdown(dropdownOptions);
+      } else {
+        closeDropdown(dropdownOptions);
+      }
+    });
+
+    const options = dropdownOptions.querySelectorAll("li");
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        options.forEach((opt) =>
+          opt.classList.remove("text-primary", "font-bold")
+        );
+        this.classList.add("text-primary", "font-bold");
+        selectedOption.textContent = this.textContent;
+        closeDropdown(dropdownOptions);
+      });
+    });
+
+    document.addEventListener("click", function (event) {
+      if (!customSelect.contains(event.target)) {
+        closeDropdown(dropdownOptions);
+      }
+    });
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!event.target.closest(".custom-select")) {
+      closeAllDropdowns();
+    }
   });
 });
